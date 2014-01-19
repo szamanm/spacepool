351ab61291748fba5d391fb9e410ba2eb486aeb2
szamanm <szamanm@github.com>
2014-01-19 16:18:11 +0100

    adding classes and vector support

--- classes/TS_vector2.js
+++ classes/TS_vector2.js
@@ -10,29 +10,29 @@ function Vec2(x_,y_)
     this.y = y_;
    
 	/* vector * scalar */
-	this.mulS = function (value){ 	return new Vec2(this.x*value, this.y*value); 		}
+	this.mulS = function (value){ 	return new Vec2(this.x*value, this.y*value); 		};
 	/* vector * vector */
-	this.mulV = function(vec_)	{ 	return new Vec2(this.x * vec_.x, this.y * vec_.y);	}
+	this.mulV = function(vec_)	{ 	return new Vec2(this.x * vec_.x, this.y * vec_.y);	};
 	/* vector / scalar */
-	this.divS = function(value)	{ 	return new Vec2(this.x/value,this.y/value); 		}
+	this.divS = function(value)	{ 	return new Vec2(this.x/value,this.y/value); 		};
 	/* vector + scalar */
-	this.addS = function(value)	{ 	return new Vec2(this.x+value,this.y+value); 		}
+	this.addS = function(value)	{ 	return new Vec2(this.x+value,this.y+value); 		};
 	/* vector + vector */
-	this.addV  = function(vec_)	{ 	return new Vec2(this.x+vec_.x,this.y+vec_.y);		}
+	this.addV  = function(vec_)	{ 	return new Vec2(this.x+vec_.x,this.y+vec_.y);		};
 	/* vector - scalar */
-	this.subS = function(value) {	return new Vec2(this.x-value, this.y-value);		}
+	this.subS = function(value) {	return new Vec2(this.x-value, this.y-value);		};
 	/* vector - vector */
-	this.subV = function(vec_) 	{	return new Vec2(this.x-vec_.x,this.y-vec_.y);		}
+	this.subV = function(vec_) 	{	return new Vec2(this.x-vec_.x,this.y-vec_.y);		};
 	/*	vector absolute */
-	this.abs = function() 		{	return new Vec2(Math.abs(this.x),Math.abs(this.y)); }
+	this.abs = function() 		{	return new Vec2(Math.abs(this.x),Math.abs(this.y)); };
 	/* dot product */
-	this.dot = function(vec_) 	{	return (this.x*vec_.x+this.y*vec_.y); 				}
+	this.dot = function(vec_) 	{	return (this.x*vec_.x+this.y*vec_.y); 				};
 	/* vector length */
-	this.length = function()	{	return Math.sqrt(this.dot(this)); 					}
+	this.length = function()	{	return Math.sqrt(this.dot(this)); 					};
 	/* distance between vectors */
-	this.dist = function(vec_)  {  	return (vec_.subV(this)).length();					}
+	this.dist = function(vec_)  {  	return (vec_.subV(this)).length();					};
 	/* vector length, squared */
-	this.lengthSqr = function() { 	return this.dot(this); 								}
+	this.lengthSqr = function() { 	return this.dot(this); 								};
 	/* 
 		vector linear interpolation 
 		interpolate between two vectors.
@@ -43,11 +43,11 @@ function Vec2(x_,y_)
 						 this.x+(vec_.x-this.x)*value, 
 						 this.y+(vec_.y-this.y)*value 
 						); 
-	}
+	};
 	/* normalize THIS vector */
 	this.normalize	= function() {
 		var vlen   = this.length();
 		this.x = this.x/ vlen;
 		this.y = this.y/ vlen;
-	}
+	};
 }
