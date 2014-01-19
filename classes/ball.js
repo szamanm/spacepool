7311a67c94f1b804316913e5f0e36d4e7a93c291
szamanm <gpiotr@gmail.com>
2014-01-19 22:20:28 +0100

    drag & drop to balls

--- classes/ball.js
+++ classes/ball.js
@@ -1,10 +1,13 @@
+var TOUCH_INFLUENCE_FACTOR = 0.3;
+var TOUCH_MOVE_FADER = 0.7;
+
 var Ball = Class.create(Sprite, {
 	initialize : function(pos, r, owner) {
 		Sprite.call(this, r * 2, r * 2);
 
 		this.r = r;
 		this.owner = owner;
-		this.speed = Vec2(0, 0);
+		this.speed = new Vec2(0, 0);
 		var surface = new Surface(2*r, 2*r);
 		surface.context.beginPath();
 		surface.context.arc(r, r, r, 0, Math.PI * 2);
@@ -19,12 +22,34 @@ var Ball = Class.create(Sprite, {
 
         this.addEventListener("enterframe", function(){
         	var coliding = world.find(this);
+        	//lower 
+        	this.futureSpeed = this.futureSpeed.mulS(TOUCH_MOVE_FADER);
         	if(coliding.length == 1)
         	   var a;
             this.pos = this.pos.addV(this.speed);
         });
         world.add(this);
+        
+        this.addEventListener('touchstart', this.startMove);
+        this.addEventListener('touchmove', this.moveByUI);
+        this.addEventListener('touchend', this.setNewSpeed);
 	},
+	startMove : function(e){
+	    this.speed = new Vec2(0,0);
+        this.futureSpeed = new Vec2(0,0);
+        this.moveByUI(e);
+	},
+	moveByUI: function(e){
+	    this.movePrev = this.pos;
+	    var newPos = new Vec2(e.x - this.r, e.y - this.r);
+        this.futureSpeed = this.futureSpeed.addV(newPos.subV(this.pos).mulS(TOUCH_INFLUENCE_FACTOR));
+        this.pos = newPos;
+	},
+    setNewSpeed: function(e){
+        this.speed = this.futureSpeed;
+    },
+	futureSpeed : new Vec2(0,0),
+	movePrev : new Vec2(0,0),
 	r : 0,
 	m : 0,
 	owner : null
