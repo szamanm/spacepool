0d8224ac23666788dd7ca45581a13329415bb45e
Neurochrom <pcichock@poczta.onet.pl>
2014-01-19 17:55:34 +0100

    Added background and stuff.

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ enchant();
 //VSprite.__proto___ = Sprite;
 
 Sprite.prototype.setPosV = function(vec_) {
-	this.prototype.x = vec_.x;
-	this.prototype.y = vec_.y;
+	this.x = vec_.x;
+	this.y = vec_.y;
 };
 
 Sprite.prototype.addToPosV = function(vec_) {
@@ -17,17 +17,24 @@ Sprite.prototype.addToPosV = function(vec_) {
 	this.y += vec_.y;
 };
 
+var world = {};
+
 
 window.onload = function(){
-    var game = new Core(320, 320);
+    var game = new Core(960, 640);
     game.fps = 60;
-    game.preload("chara1.png");
+    game.preload('background.png');
     game.onload = function(){
+    	var background = new Sprite(960,640);
+    	background.image = game.assets["background.png"];
+    	game.rootScene.addChild(background);
+    	
         var ball = new Sprite(50, 50);
-        ball.image = game.assets["chara1.png"];
+        //ball.image = game.assets["chara1.png"];
         var surface = new Surface(50, 50);
         surface.context.beginPath();
         surface.context.arc(25, 25, 25, 0, Math.PI*2, true);
+        surface.context.fillStyle = '#FFFF00';
         surface.context.fill();
         ball.image = surface;
         
@@ -35,6 +42,9 @@ window.onload = function(){
         ball.y = 0;
         
         ball.speed = new Vec2(2,1);
+        
+        
+        
         
         //ball.frame = 5;
         game.rootScene.addChild(ball);
@@ -44,6 +54,8 @@ window.onload = function(){
         	//this.x += this.speed.x;
         	//this.y += this.speed.y;
         	this.addToPosV(this.speed);
+        	
+        	
         	
             //this.frame = this.age % 2 + 6;
         });
