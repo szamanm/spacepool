1599619ac5a09a1012e10fcbd1b433305ec0816c
Paweł Cichocki <Neurochorm@github.com>
2014-01-19 16:34:31 +0100

    Vector speed.

--- main.js
+++ main.js
@@ -6,7 +6,16 @@ enchant();
 //var VSprite = {};
 //VSprite.__proto___ = Sprite;
 
-//Sprite.prototype.setPos = function()
+Sprite.prototype.setPosV = function(vec_) {
+	this.prototype.x = vec_.x;
+	this.prototype.y = vec_.y;
+};
+
+Sprite.prototype.addToPosV = function(vec_) {
+	console.log(vec_);
+	this.x += vec_.x;
+	this.y += vec_.y;
+};
 
 
 window.onload = function(){
@@ -25,17 +34,16 @@ window.onload = function(){
         ball.x = 0;
         ball.y = 0;
         
-        ball.speed = {};
-        ball.speed.x = 2;
-        ball.speed.y = 1;
+        ball.speed = new Vec2(2,1);
         
         //ball.frame = 5;
         game.rootScene.addChild(ball);
 
         ball.addEventListener("enterframe", function(){
             //this.x += 1;
-        	this.x += this.speed.x;
-        	this.y += this.speed.y;
+        	//this.x += this.speed.x;
+        	//this.y += this.speed.y;
+        	this.addToPosV(this.speed);
         	
             //this.frame = this.age % 2 + 6;
         });
